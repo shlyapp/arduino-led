08bb73420e3291d5081f866e05e581e0a1be80da
Tor07 <shlyapp.exe@gmail.com>
2021-11-26 00:37:35 +0400

    test code

--- led-arduino-sketch/led-arduino-sketch.ino
+++ led-arduino-sketch/led-arduino-sketch.ino
@@ -1,9 +1,75 @@
+#define R 10
+#define G 9
+#define B 11
+
+byte r_;
+byte g_;
+byte b_;
+
+unsigned long int current_millis;
+int value_led;
+int serial_val;
+bool raising = true;
+
+char key;
+int mode;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
+
+  analogWrite(R, 255);
+  analogWrite(G, 255);
+  analogWrite(B, 255);
+  
+  Serial.begin(9600);
+  Serial.setTimeout(5);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void smoothFlashing(int speed)
+{
+  if (millis() - current_millis > speed)
+  {
+    current_millis = millis();
+    if (raising) 
+      value_led++;
+    else
+      value_led--;
+    if (value_led >= 255 || value_led <= 0) raising = !raising;
+    analogWrite(R, 255 - value_led);
+    analogWrite(B, 255 - value_led);
+    Serial.println(value_led);
+  }
+}
 
+void loop() {
+  if (Serial.available() > 1)
+  {
+    key = Serial.read();
+    serial_val = 255 - Serial.parseInt();
+    switch (key)
+    {
+      case 'r':
+        r_ = serial_val;
+        analogWrite(R, r_);
+        break;
+      case 'g':
+        g_ = serial_val;
+        analogWrite(G, g_);
+        break;
+      case 'b':
+        b_ = serial_val;
+        analogWrite(B, b_);
+        break;
+    }
+  }
+
+  switch (key)
+  {
+    case 'e':
+      smoothFlashing(serial_val / 100);
+      break;
+  }
 }
