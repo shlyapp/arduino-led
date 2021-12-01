9c7d70d4fbc19b57683c8636b17996c220835d80
shlyapp <shlyapp.exe@gmail.com>
2021-12-01 16:08:26 +0400

    добавление формы настроек
    добавлена форма настроек для дальнейшей настройки параметров подключения и так далее

---
+++ led-controller/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace arduino_with_pc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.buttonContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(618, 12);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(170, 60);
+            this.connectBtn.TabIndex = 1;
+            this.connectBtn.Text = "button1";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            // 
+            // buttonContextMenu
+            // 
+            this.buttonContextMenu.Name = "buttonContextMenu";
+            this.buttonContextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.Location = new System.Drawing.Point(618, 188);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(170, 60);
+            this.settingsBtn.TabIndex = 4;
+            this.settingsBtn.Text = "button1";
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            this.settingsBtn.Click += new System.EventHandler(this.settingsBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 416);
+            this.Controls.Add(this.settingsBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.ContextMenuStrip buttonContextMenu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button settingsBtn;
+    }
+}
+
---
+++ led-controller/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+using led_controller;
+
+namespace arduino_with_pc
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort serialPort;
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void settingsBtn_Click(object sender, EventArgs e)
+        {
+            SettingsForm sd = new SettingsForm();
+            sd.ShowDialog();
+        }
+    }
+
+    public class Led
+    {
+        static float step = 255.0f / 91.0f;
+
+        public static int ConvertValueToPWM(float value)
+        {
+            value = value * step;
+            return (int) value;
+        }
+    }
+}
