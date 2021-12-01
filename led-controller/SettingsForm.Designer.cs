d90d8e063c66543acbb4200ebc9d7d21ed347936
shlyapp <shlyapp.exe@gmail.com>
2021-12-01 16:39:30 +0400

    выбор com порта
    сделан выбор com порта и сохранения его номера

--- led-controller/SettingsForm.Designer.cs
+++ led-controller/SettingsForm.Designer.cs
@@ -29,12 +29,60 @@ namespace led_controller
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // portBox
+            // 
+            this.portBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(270, 40);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(121, 33);
+            this.portBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(40, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Порт подключения";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveBtn.Location = new System.Drawing.Point(212, 241);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(179, 58);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Сохранить";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(434, 311);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portBox);
+            this.Name = "SettingsForm";
             this.Text = "SettingsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveBtn;
     }
 }
--- led-controller/SettingsForm.cs
+++ led-controller/SettingsForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace led_controller
 {
@@ -15,6 +16,14 @@ namespace led_controller
         public SettingsForm()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            portBox.Items.AddRange(SerialPort.GetPortNames());  
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            Settings.connectPort = portBox.Text;
         }
     }
 }
